cd66439ed2ebf9ff97a7a05430e114a93016ee51
Thanh Do <thanhstp99@gmail.com>
2021-04-26 08:13:51 +0700

    vai lol

--- db/migrate/20210421134837_create_cms_categories.rb
+++ db/migrate/20210421134837_create_cms_categories.rb
@@ -1,12 +1,12 @@
 class CreateCmsCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :cms_categories do |t|
-      t.integer :ParentID, :null => true
+      t.integer :ParentID :null => true
       t.string :Name_vi :null => true
-      t.string :Name_en, :null => true
+      t.string :Name_en :null => true
       t.string :Slug_vi :null => true
-      t.string :Slug_en, :null => true
-      t.integer :Index, :null => true
+      t.string :Slug_en :null => true
+      t.integer :Index :null => true
 
       t.timestamps :null => true
     end
